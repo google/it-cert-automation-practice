a5023f6f4a945b44e40c776257421925a747845c
Shigeo Maruse <shigeo.maruse@africanbeat.org>
2020-02-01 17:46:51 +0000

    Refactored regex to exclude numerices from the start of the string and removed the equivalent if clause. Close #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,12 +13,9 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-zA-Z0-9][a-zA-Z0-9._]{2,}$', username):
-        return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
-    return True
+    if re.match('^[a-zA-Z][a-zA-Z0-9._]*$', username):
+        return True
+    return False
 
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
