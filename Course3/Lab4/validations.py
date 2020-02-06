4b0bd713c20428777a5f693943de12ed4c08c2db
Kuosheng Chang <student-01-6ed7a41c567f@linux-instance.c.qwiklabs-gcp-01-f9cfa31f12a0.internal>
2020-02-06 00:54:20 +0000

    Closes #1 Removed redundant logic, changed first character to [a-z] only in validate_user() in validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,11 +13,10 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
+    # except first character, which must be a letter
     if not re.match('^[a-z][a-z0-9._]*$', username):
         return False
-    # Usernames can't begin with a number
-    #if username[0].isnumeric():
-    #    return False
+
     return True
 
 print(validate_user("blue.kale",3)) #true
