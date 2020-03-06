630c25fabea4f1ff9d3c3f81bacdbecaf391d652
goldaccount <katarinahri@gmail.com>
2020-03-06 03:21:45 +0000

    Closes: #1 Fix username check

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,7 +8,8 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+    if (username[0] == "_" or username[0] == "."):
+        return False
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
