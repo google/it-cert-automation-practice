4f7a8469eba8a7d5aa077802baacd84138d2170a
sudha <sudha04.a@gmail.com>
2020-03-12 19:53:22 +0000

    closes #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -20,12 +20,15 @@ def validate_user(username, minlen):
         return False
     # To check for first character to be alpha
     if username[0].isalpha():
+        return True
+    else:
         return False
     return True
-print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("blue.kale", 3)) # True
-print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+
 
 
 
