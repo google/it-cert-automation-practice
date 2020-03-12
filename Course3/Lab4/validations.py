9127ee8d2cdd9618f519584b7a16fd3de55506f6
nicola-co <62106403+nicola-co@users.noreply.github.com>
2020-03-12 20:48:52 +0000

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -16,7 +16,7 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames must begin with a letter
-    if not re.match('[a-zA-Z]', username[0]):
+    if not username[0].isalpha():
         return False
     return True
 
