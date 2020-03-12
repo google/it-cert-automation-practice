3a7476e84f0eda4a50e7eb1643506304a8b1a93f
conced <cedric_conan@hotmail.com>
2020-03-12 16:33:13 +0000

    Closes: #1
    Updated validations.py python script

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -12,6 +12,11 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
+    # Username first letter must be alpha
+    if username[0] in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","z"]:
+        pass
+    else:
+        return False
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
@@ -20,5 +25,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
