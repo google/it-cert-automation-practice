28305eceb359fcbf4aa24b93547ac45076c5b824
Evens <evensmetellus@gmail.com>
2020-04-05 17:25:07 +0000

    Make a difference by fixing the code

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,9 +1,10 @@
+
 #!/usr/bin/env python3
 
 import re
 
 def validate_user(username, minlen):
-    """Checks if the received username matches the required conditions."""
+   #Checks if the received username matches the required conditions.
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
@@ -18,7 +19,15 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    return True
+    # Check the letter
+    if username[0].isalpha():
+      return True
+
+    return False
 
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+    
