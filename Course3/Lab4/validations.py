001ee2132fc57aa451abfc75808d862d60fe63fd
XumingMa <xuming.ma.sr@gmail.com>
2020-04-07 03:53:47 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,6 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # Fix
     if username[0].isalpha():
         return False
     return True
