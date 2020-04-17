46a8b55021a6f052cb46a46cf60291970a1f7c69
nurlan2002 <45170430+nurlan2002@users.noreply.github.com>
2020-04-17 21:02:53 +0300

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -12,6 +12,10 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
+    
+    if not username[0].isalpha():
+        return False
+    
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
