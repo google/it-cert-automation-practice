4c69e3086eec8a40eef430bc3acfda2c452e1269
akakafonis <akakafonis@gmail.com>
2020-05-03 19:04:39 +0000

    Changed the regex so that it only allows lowercase alphabet characters as the first username character.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,13 +13,7 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
-        return False
-    # Usernames can't begin with a number
-   # if username[0].isnumeric():
-       # return False
-    #isalpha will cover all the above cases
-    if not username[0].isalpha():
+    if not re.match('^[a-z][a-z0-9._]*$', username):
         return False
     return True
 
