af0cab856a06da26610371429b349d14a2d9cce7
yangsihrdev <yangsihrdev@gmail.com>
2020-05-23 14:32:06 +0000

    test Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import re
 
 def validate_user(username, minlen):
@@ -22,7 +21,7 @@ def validate_user(username, minlen):
     # Usernames must begin with character
 
 print(validate_user("blue.kale", 3)) # True
-print(validate_user("0blue.kale", 3)) # Currently True, should be False
+print(validate_user("2blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
 print(validate_user("1red_quinoa", 4)) # Currently True, should be False
 
