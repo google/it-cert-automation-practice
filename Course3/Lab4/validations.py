4dfc60173e89fcc37fd31c3aacfddf66bc7f1104
Ali18SW66 <49496397+Ali18SW66@users.noreply.github.com>
2020-06-20 12:38:11 +0500

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,9 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    #User name should be a letter
+    if not username[o].isalpha():
+        return False
     return True
 
 
