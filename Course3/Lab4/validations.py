5fea25352972312ab98c75bbe11860ec23cd85c1
pandya keval <67450371+keval2232@users.noreply.github.com>
2020-06-26 18:23:34 +0530

    I added a condition that fix  the bug

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -3,12 +3,13 @@
 import re
 
 def validate_user(username, minlen):
-    """Checks if the received username matches the required conditions."""
+    """Checks if the received username matches the required conditions.""" re
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+    if not re.match('^[^._]',username[0]):
+        return False
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
