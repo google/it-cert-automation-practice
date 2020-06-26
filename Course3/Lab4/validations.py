d0e7d7cd6b3b4e2c97e708dd78204f7c744b3a0e
ZeeshanC <42517897+ZeeshanC@users.noreply.github.com>
2020-06-27 01:19:56 +0530

    Updated validations.py python scripts

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-
 import re
-
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
