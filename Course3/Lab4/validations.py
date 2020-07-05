df0de61bbd961924ee95edee99e6238e540bb20f
GP Kulam <kulamgangadhar03@gmail.com>
2020-07-05 04:24:53 +0000

    Closes: #1 update validations.py python script add some tests which are not included before

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,14 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # username should not start with a special character
+    if((username[0] == '_' ) or (username[0]<'a' and username[0]>'z') or (username[0]<'A' and username[0])):
+        return False
+
     return True
 
+print(validate_user("blue.kale", 3)) #True
+print(validate_user(".blue.kale", 3)) # Currently True, should be false
 
-
+print(validate_user("red_quinoa", 4)) #True
+print(validate_user("_red_quinoa", 4)) #Currently true should be false
