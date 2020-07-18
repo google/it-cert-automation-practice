ee47467b65364445a2d9d147874d170a05937d09
yashsam99 <yash.samdani99@gmail.com>
2020-07-18 09:13:31 +0000

    Closes: #1 Updated validations.py python script fixed issue

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -19,9 +19,9 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
-    if not  re.match('^[a-z]',username[0]):
-      return False
-    return True
+    if username[0].isalpha():
+   	 return True
+    return False
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
