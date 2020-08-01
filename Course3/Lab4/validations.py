ece748df7bb7ff7f9865cad5901571bd01fa6806
Neeraj <user@example.co>
2020-08-01 17:51:45 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -6,7 +6,7 @@ def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
-    if minlen < 1:
+    if minlen < 2:
         raise ValueError("minlen must be at least 1")
     
     # Usernames can't be shorter than minlen
@@ -20,5 +20,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
