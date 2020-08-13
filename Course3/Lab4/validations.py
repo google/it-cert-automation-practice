b1412e9fc6a82f2313472e83e3b908466e833abb
Mohit Kumar <mohitk02082000@gmail.com>
2020-08-13 09:49:42 +0000

    Closes: #1 made some changes

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -12,7 +12,8 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores and first character is always a letter
+    # Usernames can only use letters, numbers, dots and underscores and 
+    # first character is always a letter
     if not re.match('^[a-z0-9][a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
