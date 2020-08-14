7b60af431fa8a8db2cadad92a0a8e76b77ddc4c6
Gabriel Cesário <gcesario031@gmail.com>
2020-08-14 15:02:00 +0000

    Updated validations.py script.
    Fixed the validate_user function. The function now validates names
    starting with non-letters characters.
    
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,16 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+
+    first_char = username[0]
+
+    if not (first_char >= 'a' and first_char <= 'z' or first_char  >= 'A' and first_char <= 'Z'):
+        return False
+
     return True
 
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
