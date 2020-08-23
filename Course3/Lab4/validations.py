6b76d7f0fa0831d8da3548cb9126a586055cdf30
dean <myemail@myaddress.com>
2020-08-23 10:38:25 +0000

    Closes: #1 typo fix

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # Usernames must begin with a letter
+    # Usernames must begin with a letter only
     if not username[0].isalpha():
         return False
     return True
