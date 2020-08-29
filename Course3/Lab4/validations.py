3dde6618672c70184d0828e085b6f326ba2b1988
Edmundo <mundo.evo@gmail.com>
2020-08-29 14:10:02 +0000

    Updated validations.py Fixes problem from fitst letter input in function validate_user in validations.py Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,12 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Usernames can't start with something else but a letter
+    if re.match('^[0-9._]',username):
+        return False
     return True
 
-
-
+print(validate_user("blue.kale", 3)) #True
+print(validate_user(".blue.kale", 3)) #Should be False
+print(validate_user("red_quinoa", 4)) #True
+print(validate_user("_red_quinoa", 4)) #Should be False
