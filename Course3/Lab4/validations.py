3e774a97a36b074ebf8e6454fe1cb298f603d880
noName <noName@ex.com>
2020-08-30 06:08:27 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import re
@@ -16,9 +17,14 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+    ch=username[0]
+    if (ch.isnumeric() or ch=='.' or ch=='_'):
         return False
     return True
 
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False#!/usr/bin/env python3
 
