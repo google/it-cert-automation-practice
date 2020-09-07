f265787165fd0e69c6adce4090933cad8984552f
PawelBorkar <pawelborkar@gmail.com>
2020-09-07 08:28:20 +0000

    Fixed validations.py
    Added a condition to check whether the username starts with a "." or an "_"
    
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Username can't begin with a period or an underscore
     if re.match('^[._]*$',username[0]):
         return False
     return True
