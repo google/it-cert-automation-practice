e43c8992c07fe8ca3f0ae1cdea90b229e881aa60
Pranav Soni <57292159+ps428@users.noreply.github.com>
2020-09-08 09:46:50 +0530

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,12 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    if username[0]=='.':
+        return False
+        
+    if username[0]=='.':
+        return False
+    
     return True
 
 
