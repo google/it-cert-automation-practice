ac0442fb3d70703d3250f899ee227d801e59605c
vybhav72954 <vybhavchaturvedi@gmail.com>
2020-09-15 02:01:46 +0000

    Updated the If block in validation.py
    Closes: #1
    Fixed the incorrect behaviour.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,6 +4,7 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
+    valid = True
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
@@ -11,14 +12,25 @@ def validate_user(username, minlen):
     
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
-        return False
+        valid = False
+        return valid
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
-        return False
+    elif not re.match('^[a-z0-9._]*$', username):
+        valid = False
+        return valid
     # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
-    return True
-
+    elif username[0].isnumeric():
+        valid = False
+        return valid
+    elif not username[0].isalpha(): 
+        valid = False
+        return valid
+    else:
+        valid = True
+        return valid
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
