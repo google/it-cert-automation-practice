bc4e3bd74f8f7fea0bed77f70745d79f879cfd68
Ishan Joshi <53286842+Ishan25j@users.noreply.github.com>
2020-09-21 13:59:57 +0530

    Update validations.py
    Regex modification 1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,14 +9,12 @@ def validate_user(username, minlen):
     if minlen < 1:  
         raise ValueError("minlen must be at least 1")
     
-    # Usernames can't be shorter than minlen
-    if len(username) < minlen:
-        return False
-    # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z][a-z0-9._]*$', username): #USername should always start with charactor.
-        return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
+    """
+    Usernames can't be shorter than minlen
+    Username should be not numeric
+    Username should always starts with charactor and should consists of charactors, numbers, '.', '_'
+    """
+    if (len(username) < minlen) or (not re.match(r'^[a-z][a-z0-9._]*$', username)) or (username[0].isnumeric()): # changes in Regex and remove the repetition of code 
         return False
     return True
 
