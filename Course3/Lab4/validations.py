910152d0700fb2e479158dec459fc6f50f871583
Purvish Khalpada <72291069+thepurvish@users.noreply.github.com>
2020-10-03 09:45:26 +0000

    Added the comment before regex to describe the change
    Closes: #1
    Updated validations.py python script.
    Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,6 +13,7 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
+    # Fixed the re so it ensures that first character is always a small letter.
     if not re.match('^[a-z][a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
@@ -24,4 +25,3 @@ print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
 print(validate_user("_red_quinoa", 4)) # Currently True, should be False
-
