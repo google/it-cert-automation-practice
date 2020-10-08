0a1ba527659e058ee4a17c6b33fa6022e23565a3
Mitadru Datta <63799458+MitadruDatta@users.noreply.github.com>
2020-10-08 23:04:39 +0530

    Updated validations.py and corrected username problem

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -16,7 +16,7 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can start only with alphabets
-    if not re.match('^[a-z]*$', username):
+    if not re.match('^[a-z]*$', username[0]):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
