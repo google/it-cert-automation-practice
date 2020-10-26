37de842418172a21b17cc4708191999fc2c02586
Juerodriguez <stylorj@gmail.com>
2020-10-26 16:26:27 +0000

    Closes: #1 Updated Validations.py python script THis is the fix verific

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -21,7 +21,7 @@ def validate_user(username, minlen):
     
     if username[0] == '.' or username[0] == '_':
         return False
-    return True
+    return True  
 
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
