cf7cf24b79aba372c33a0d8813b3e1079ace1d17
geygrissom <greygrissom@me.com>
2020-10-27 20:53:53 -0700

    Fixed the calidate user funciton

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -2,14 +2,17 @@
 
 import re
 
+forbidden = ['_', '-', '*', '%', '#', '&', '.', '!', '$']
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+
     # Usernames can't be shorter than minlen
+    if username[0] in forbidden: 
+        return False
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
@@ -18,7 +21,15 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    elif bool(re.match('^[A-Za-z]+', username)):
+        pass
     return True
 
+
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
