5de72ba87b612ee9cc7513020ab656b56a7a2324
Alex <alex@gmail.com>
2020-11-03 13:40:59 +0000

    Closes: #1 Finally all fixed.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # Fixes an issues
+    # Fixes an issue
     if not username[0].isalpha():
         return False
     return True
