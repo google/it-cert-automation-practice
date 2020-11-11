f4df716bd4da72540c508f1fb2cd59ff260604a5
Trong Nghia <trongnghia.nguyen@outlook.com>
2020-11-11 16:52:26 +0000

    Add more comment Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # TNT code
     if not re.match('[a-zA-Z]',username[0]):
         return False
     return True
