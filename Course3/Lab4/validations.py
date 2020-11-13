40d7cbdb6d6af79834535bfb7ac37b7abc690dbf
Madhu <53974577+madhulakkoju@users.noreply.github.com>
2020-11-13 17:03:18 +0000

    done Closes: #1 fixed

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-
+#ALl modifications done and ready to go
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
