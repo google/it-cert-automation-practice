6c7af726b1ca78d04f6fa6352a315a50c09f3e4a
Shubham <shubham0510@gmail.com>
2020-11-28 03:44:45 +0000

    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -22,6 +22,7 @@ def validate_user(username, minlen):
     if not re.match('[a-z]', username[0]):
         return False
     return True
+
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
