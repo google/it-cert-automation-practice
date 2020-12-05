678c45543127cf8f109da46f7a3e4afef5284e47
Devansh Shukla <46221293+Devansh252@users.noreply.github.com>
2020-12-05 21:26:04 +0530

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+#Part of a course from coursera
 import re
 
 def validate_user(username, minlen):
@@ -19,7 +19,7 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
-print(validate_user("blue.kale ", 3)) # True
+print(validate_user("blue.kale ", 3)) # True 
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 #print(validate_user("red_quinoa", 4)) # True
 print(validate_user("_red_quinoa", 4)) # Currently True, should be False
