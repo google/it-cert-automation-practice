d9e5e4146fb4dff558acd9ef5f2f06902833aadb
Devansh Shukla <46221293+Devansh252@users.noreply.github.com>
2020-12-05 21:45:26 +0530

    Update hello_cloud.py

--- Course5/Lab3/hello_cloud.py
+++ Course5/Lab3/hello_cloud.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A simple Hello World type app which can serve on port 8000.
+"""A simple Hello World type app which can serve on port 3000.
 Optionally, a different port can be passed.
 
 The code was inspired by:
