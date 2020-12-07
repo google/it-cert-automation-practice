9d9a21cd290443acdfdcdaae87dced4423952695
IfebuemeDaniel <ifebuemedaniel7@gmail.com>
2020-12-07 18:12:05 +0000

    Added an if condition to check if the first character in username variable is a bad character Closes:#1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,6 +4,7 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
+    bad= """_-=!"£$%&.*()_+ """
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
@@ -18,7 +19,11 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    if username[0] in bad:
+        return False
     return True
-
-
+print(validate_user("blue.kale",3))
+print(validate_user(".blue.kale",3))
+print(validate_user("red_quinoa",4))
+print(validate_user("_red_quinoa",4))
 
