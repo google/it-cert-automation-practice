7546cf8c1136f65855d255dc37bd8f041000e92c
Name <user@example.com>
2020-12-08 12:35:30 +0000

    add validation tests

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,6 +4,8 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
+    if re.match(r"^[A-Za-z]", username) is not None:
+        return False
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
@@ -18,7 +20,10 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+
     return True
 
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
