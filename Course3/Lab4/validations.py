21fa3e47b58d5637f2c1e6200060ad1f0f23574a
Wo0dsi3 <donnchadh.woods@bt.com>
2020-12-14 21:33:16 +0000

    Closes: #1 Updated the validation script. I changed the regex match pattern Validation working as expected now.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -12,7 +12,7 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores
+    # Usernames can only use letters, numbers, dots and underscores , first character group needs to be letter only
     if not re.match('^[A-Za-z][A-Za-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
