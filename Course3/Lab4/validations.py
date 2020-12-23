2ff0a31cc837bad310983a9fb07d04c86cfe33d5
ashanuoc <student-04-b066bec6a586@linux-instance.us-central1-a.c.qwiklabs-gcp-04-fc592a27959d.internal>
2020-12-23 17:54:03 +0000

    Closes: #1 Import the string module to get the speacial characters and check it with the first letter in username

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import re
+import string
+
+special_chars = string.punctuation
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
@@ -16,9 +19,13 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+    if username[0].isnumeric() or username[0] in special_chars :
         return False
     return True
 
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
