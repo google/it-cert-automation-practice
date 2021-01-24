5b8e20779854be9e543139fbae99afc8ab313ea2
Hardev Sanghera <devsanghera@gmail.com>
2021-01-24 05:14:08 +0000

    I fookin fixed it!

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,6 +9,10 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
+    # Usernames must begin with a number
+    if not username[0].isalpha():
+        return False
+    return  True
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -19,6 +23,11 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
 
