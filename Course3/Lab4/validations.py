1c4dc56cd1ff2b1a1e0c9e14dbf9a999e03a2bd0
Sanchit <sanchit@gmail.com>
2021-01-26 08:43:02 +0000

    Nothing new her

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -19,6 +19,9 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
-
+print(validate_user("blue.kale",3))#True
+print(validate_user(".blue.kale",3))#Currently true, should be false
+print(validate_user("red_quinoa",4))#True
+print(validate_user("_red_quinoa",4))#Cureently true,should be false
 
 
