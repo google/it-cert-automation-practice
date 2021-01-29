77f844696b331996a39e57e2bebbfdcfd3aae1c1
asdfklhjasldjkfghasd <lshjkasdf@gmail.com|>
2021-01-29 17:18:05 +0000

    Fixed validations file

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,7 +4,7 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
-    if type(username) != str:
+    if not re.match("^[a-zA-Z]", username):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
@@ -20,5 +20,7 @@ def validate_user(username, minlen):
         return False
     return True
 
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
