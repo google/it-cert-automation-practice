532af715ea5b356103521e2bf3a5978adce68cf3
Mark George <markgeorgekamel@outlook.com>
2021-02-20 03:33:21 +0000

    adding new functionality
    checks if the username begins with "_" or "." and returns False in both these cases
    
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,14 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Usernams  can't begin wtih "_" or "."
+    if username.startswith("_"):
+        return False
+    if username.startswith("."):
+        return False
     return True
 
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
