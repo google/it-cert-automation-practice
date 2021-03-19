35c95962890e4bdfe8f60b74b7a20a20246c2292
ankit agrawal <ankitstudy9@gmail.com>
2021-03-19 23:46:01 +0530

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -3,7 +3,7 @@
 import re
 
 def validate_user(username, minlen):
-    """Checks if the received username matches the required conditions."""
+    """Checks if the recevived username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
