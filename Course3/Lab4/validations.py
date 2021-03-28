f425f802284036d5419190cea3e98aec71f7fec0
Sarma <Sarma_vvsss@hotmail.com>
2021-03-28 13:45:17 +0000

    made mofigications to the validations py by adding a check to see if first letter is a letter else it will return false Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+import string
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
@@ -18,7 +19,14 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    return True
-
+    # Usernames can't begin with non special character
+    if username[0].isalpha():
+        return True
+    return False
+    
+print(validate_user("blue.kale",3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
