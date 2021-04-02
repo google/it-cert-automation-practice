1ee7d4e4c248cf571b5220be5c128917cb410757
Andrew Miller <chumley207@gmail.com>
2021-04-02 02:45:05 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,17 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Usernames can't begin with dots
+    elif username[0] == '.':
+        return False
+    # Usernames can't begin with underscores
+    elif username[0] == '_':
+        return False
     return True
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # False
 
 
