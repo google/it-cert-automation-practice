47cafa2280bb5f51d0464480952a5b89a446d864
Sab <abc>
2021-04-04 09:08:28 +0000

    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    #comment
     if not re.match('[a-z]',username[0]):
         return False
     return True
