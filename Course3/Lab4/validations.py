8f510bb60a2ecd95481484c81c067dd670f28bb3
Micha <mgengler71@gmail.com>
2021-04-05 06:11:15 +0000

    Fixing character check
    Closes: #1
    Updated validations.py python script.
    Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import re
+import string
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
@@ -8,6 +9,10 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
+    ALPHA = string.ascii_letters
+    if not username.startswith(tuple(ALPHA)):
+        return False 
+
     
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
@@ -20,5 +25,9 @@ def validate_user(username, minlen):
         return False
     return True
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
