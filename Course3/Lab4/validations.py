a76d58a4daae1a5f4a4cc1951c623a91d9509d03
studTon <bra.17.ess@gmail.com>
2021-04-27 20:22:44 -0300

    I fixed my code from preview pull request

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,16 +13,17 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    if not re.match('^[a-z.0-9._]*$', username):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
+        print(validate_user(".blue.kale", 3)) # False
+        print(validate_user("_red_quinoa", 4)) # False
         return False
-	print(validate_user(".blue.kale", 3)) # False
-	print(validate_user("_red_quinoa", 4)) # False
-    return True
-    print(validat_user("blue.kale", 3)) # True
-    print(validate_user("red_quinoa", 4)) # True
+    else:
+        print(validat_user("blue.kale", 3)) # True
+        print(validate_user("red_quinoa", 4)) # True
+        return True
 
 
 
