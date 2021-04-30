3e151d736897ae506f37676976395bbca1af9e17
Logan <student-00-4d8a8a72f97f@linux-instance.us-central1-a.c.qwiklabs-gcp-00-0131c19e8695.internal>
2021-04-30 18:38:18 +0000

    Closes: #1 Updated validations.py python script Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -3,6 +3,7 @@
 import re
 
 def validate_user(username, minlen):
+    dont_use = ['.','_']
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
@@ -18,7 +19,12 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    if username[0] in dont_use:
+        return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
