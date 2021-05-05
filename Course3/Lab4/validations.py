8983856d23f5cd3b3fb752eb975701d50ebbd4c2
Jenn <jenniferrozenich@gmail.com>
2021-05-05 03:29:44 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py. Using a for loop over the check on first letter is alpha function.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,7 +8,13 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+    for letter in username:
+        if letter.isalpha():
+           return True
+        return False 
+
+    #if not  username.startswith(username.isalpha()):
+    #    return False
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -20,5 +26,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
