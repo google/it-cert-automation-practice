a13894b2500f19abf7c8af1bd20a21af44968d78
sayeemur <mursalin.eee@gmail.com>
2021-05-15 09:17:25 +0000

    Closes: #1 validation.py file was absent. now it has been added.

---
+++ Course4/Lab4/validation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import re
+
+def validation_user(username, minlen):
+  if type(username) != str:
+    raise TypeError("username must be a string")
+  if minlen<1:
+    raise ValueError("minlen must be at least 1")
+  if len(username) < minlen:
+    return False
+  if not re.match('^[a-z0-9._]*$', username):
+    return False
+  if username[0].isnumeric():
+    return False
+
+print (validate_user("blue.kale" , 3)) #True
+print (validate_user(".blue.kale" , 3)) # Currently True
+print (validate_user("red_quinoa" , 4)) #True
+print (validate_user("_red_quinoa" , 4)) # Currently True
+
+
+
+
+
