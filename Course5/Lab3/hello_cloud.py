54fdfe877453a95386242111e3456a3a76b1c1f5
Gangadhar P Kulam <56226081+GP-Kulam@users.noreply.github.com>
2021-05-21 22:03:25 +0530

    Update hello_cloud.py

--- Course5/Lab3/hello_cloud.py
+++ Course5/Lab3/hello_cloud.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# Thank you for nice tutorials
 """A simple Hello World type app which can serve on port 8000.
 Optionally, a different port can be passed.
 
