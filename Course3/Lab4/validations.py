698c7356351249feefb0f6d425f55fa5c14568f0
Dipankar Layek <dipankar.layek@gmail.com>
2021-06-01 22:01:08 +0530

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -16,7 +16,7 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+    if if not re.match('^[a-z0-9]', username):
         return False
     return True
 
