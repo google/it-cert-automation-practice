abbc8f7e8886ceaebaad247ecd07facc3834bafc
Mikael Helin <mikael.helin@gmail.com>
2021-06-23 18:25:06 +0000

    another one

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -19,10 +19,3 @@ def validate_user(username, minlen):
     if not re.match('^[a-z]',username[0]):
         return False
     return True
-
-
-
-print(validate_user("blue.kale", 3)) # True
-print(validate_user(".blue.kale", 3)) # Currently True, should be False
-print(validate_user("red_quinoa", 4)) # True
-print(validate_user("_red_quinoa", 4)) # Currently True, should be False
