0b234a17d8f9bf3a938a115e507e54177ec8723e
student-00-f8183f1443c3 <daniele.rigon@hotmail.it>
2021-06-27 20:08:22 +0000

    Fix check username first char Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -2,6 +2,15 @@
 
 import re
 
+def check_first_char(char):
+    """ Check if the first char is a letter """
+
+    pattern='^[a-zA-Z].*'
+    res = re.search(pattern,char)
+    if res is None:
+        return False
+    return True
+
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
@@ -12,6 +21,10 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
+
+    if not check_first_char(username):
+        return False
+
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
@@ -20,5 +33,9 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+print(validate_user("@lele2182",4))
 
