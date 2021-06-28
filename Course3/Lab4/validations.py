a94a6d90bca44eeb4bbb07b32eaa7713ea8924b0
Ricardo Avendano <raavendanp@eafit.edu.co>
2021-06-28 02:19:32 +0000

    Fixing validation.py Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,6 +9,10 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
+
+
+    if not re.match('^[a-zA-Z]*$', username[0]):
+        return False
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -21,4 +25,7 @@ def validate_user(username, minlen):
     return True
 
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
