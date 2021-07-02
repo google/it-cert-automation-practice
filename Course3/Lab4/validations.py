0c2b9532666828de89038830194cf46e09539470
TUMUSHIMIRE Yves <ytumushimire@truststamp.net>
2021-07-02 12:46:38 +0000

    more changes commit

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    """ my changes """
+    """ my changes hb """
     if username[0] in ['.', '_']:
         return False
     return True
