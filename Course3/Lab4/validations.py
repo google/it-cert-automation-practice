7f2864354e32eb23d1a3e0ef2c9fde0992af14bf
muleraja01 <mulerajasekharreddy01@gmail.com>
2021-07-16 02:03:03 +0000

    updatedvalidations.py python script

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -7,18 +7,22 @@ def validate_user(username, minlen):
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
-        raise ValueError("minlen must be at least 1")
-    
+        raise ValueError("minlen must be at least 1") 
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
-        return False
+	 return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
-        return False
+         return False
+    elif not username[0].isalpha():
+	 return False
     return True
 
-
+print(validate_user("blue.kale",3))#True
+print(validate_user(".blue.kale",3))#currrently True, should be False
+print(validate_user("red_quinoa",4))#True
+print(validate_user("_red_quinoa",4))#Currenlty True, should be False
 
