9f8f290973ab69e381bc8d80602399a45a36957e
Naman <mymail.naman@gmail.com>
2021-07-19 21:06:18 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import re
@@ -20,5 +21,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
