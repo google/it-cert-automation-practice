c4fee3ce64e0f95f1463ca6e5f4e9641ca890fbe
Cassie Knight <cassie.haas503@gmail.com>
2021-08-04 11:14:16 +0000

    Closes: #1 Added a check to ensure the first character is alpha.
    This check uses a list to exclude bad characters called badchars. Right now the list is only '.' and '_' because we are checking against only two usernames. Feel free to add any characters to the badchars list that are necessary for the test to succeed.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,6 +9,10 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
+    # Usernames must start with special characters.
+    badchars = [".", "_"]
+    if username[0] in badchars:
+        return False
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -20,5 +24,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
