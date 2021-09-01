35326e581fe471d912fb76eb5a5194318eeac580
Naiwrit Mullick <naiwrit.wrick@gmail.com>
2021-09-01 14:58:48 +0000

    Addded a check condition that the first character must always be a character
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # Username must begin with a character
+    # Usernames must begin with a character
     if not username[0].isalpha():
         return False;
     return True
