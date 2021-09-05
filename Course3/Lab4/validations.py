e4bfe217555bd7d542675dd97994ba2ae9a65e07
GiZZoR <gismo9@gmail.com>
2021-09-05 15:49:52 -0500

    Update validations.py
    Fixed comment that usernames cannot start with dot or underscore.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -15,7 +15,7 @@ def validate_user(username, minlen):
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
-    # Usernames can only use letters, numbers, dots and underscores
+    # Usernames cannot start with . or _
     if username[0] == "." or username[0] == "_":
         return False
     # Usernames can't begin with a number
