57bcfc36f33dd82f95292362ef5d0567e6e9b0e6
Alin1911 <andreialin2324@yahoo.com>
2021-09-21 09:22:46 +0000

    add additional conditional checks to validate the first character doesn't start with either of the forbidden characters
    Closes: #1
    Updated validations.py python script.
    Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,16 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Usernames can't start with fobian character
+    fobian_cha = ['.','_','#']
+    for chaa in fobian_cha:
+        if username[0] == chaa:
+            return False
     return True
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
 
