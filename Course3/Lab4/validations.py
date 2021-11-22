2739fcd6d45fbdab97f86213d1b2c2165dfbbe8b
Michael Roberts <michaelroberts9000@gmail.com>
2021-11-21 20:27:22 -0500

    fixed logic to do all checks every time.
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,16 +9,21 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
+    valid = True
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
-        return False
+        valid = False
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
-        return False
+        valid = False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
-    return True
+    if not username[0].isalpha():
+        valid = False
+    return valid
 
 
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
