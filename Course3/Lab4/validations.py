968da9efc784a7d466dde05ebf6b72d18bde302e
J-Lindsley <josiah.lindsley@outlook.com>
2021-12-09 21:45:08 +0000

    Closes #1 Updated validations.py python script. Fixed the behavior of validate_user function

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -17,6 +17,7 @@ def validate_user(username, minlen):
         return False
     # Usernames can't begin with a number
     if username[0].isalpha():
+    # Only a letter for first character
         return True
     return False
 print(validate_user("blue.kale", 3)) # True
