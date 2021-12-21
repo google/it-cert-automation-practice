648453ba070c18faccb4d665bf8cf22f23d319d4
vishal <vishal126sw@gmail.com>
2021-12-21 11:58:54 +0000

    added space Close: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -21,6 +21,9 @@ def validate_user(username, minlen):
     if(username[0] not in list("abcdefghijklmnopqrstuvwxz")):
         return False
     return True
+
+
+
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
