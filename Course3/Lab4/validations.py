a368493d41f84656136592fc115400e41f6e3ebe
Mikhail-V-Popov <misha_popov@mail.ru>
2022-01-30 13:45:03 +0000

    Close #18521
    Remove test lines.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -16,9 +16,3 @@ def validate_user(username, minlen):
     if not re.match('^[a-zA-Z][a-z0-9._]*$', username):
         return False
     return True
-
-
-print(validate_user("blue.kale", 3)) # True
-print(validate_user(".blue.kale", 3)) # Currently True, should be False
-print(validate_user("red_quinoa", 4)) # True
-print(validate_user("_red_quinoa", 4)) # Currently True, should be False
