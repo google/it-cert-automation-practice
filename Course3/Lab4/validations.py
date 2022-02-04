181c0f799cb20b4fcba4f7010e8ac67def97bf3e
Ognjen <ognjen.m.vukovic@gmail.com>
2022-02-04 01:08:29 +0000

    Checks if the username starts with . or _
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Usernames can't begin with dots or underscores
+    if re.match(r'^[._]', username):
+        return False
     return True
-
-
-
