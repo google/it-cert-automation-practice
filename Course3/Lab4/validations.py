a1f7c5481c148d2b138c7bccedfbee08deec0ac2
prathimahebri <pratha276@gmail.com>
2022-02-27 20:27:36 +0000

    closes #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -21,7 +21,7 @@ def validate_user(username, minlen):
     if username[0].isalpha():
        return True
     return True
-print(validate_user("blue.kale", 3)) # True
+print(validate_user("blue.kale", 3)) # true
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
 print(validate_user("_red_quinoa", 4)) # Currently True, should be False
