172d92683240b45f8418cea4e76ea073f0e2a4e4
Aulia <auliaepa@gmail.com>
2022-03-05 02:29:39 +0000

    Start with letter
    Closes: #1
    Updated validations.py python script.
    Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -12,8 +12,8 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9][a-z0-9._]*$', username):
+    # Usernames can only use letters, numbers, dots and underscores, and stars with letter
+    if not re.match('^[a-z][a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
