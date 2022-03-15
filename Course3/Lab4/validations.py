609f62e265436feb55ec587ddb1fbe244e0f8fd6
Amos907 <amosmaina907@gmail.com>
2022-03-15 16:30:56 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-
+#small changes
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
