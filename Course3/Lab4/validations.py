e870b0c3794ea21ec3febc600ddadfb80de1332d
ic-33 <charbanou.ilias@gmail.com>
2022-04-11 00:01:49 +0000

    fixed validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -16,9 +16,12 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+    if username[0].isnumeric() or username[0]=="." or username[0]=="_":
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
