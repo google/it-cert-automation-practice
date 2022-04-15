4a0b2bfcb9c1cb9b8416c333ac4cd1bc94c5e97e
Yasser <yasser.toumi@medtech.tn>
2022-04-15 21:36:50 +0000

    just work

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-
+#just a comment
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
