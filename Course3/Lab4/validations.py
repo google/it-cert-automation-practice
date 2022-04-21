77a44b3a320140ee74ec06003289b416d63c7ab0
vvkalkundri <vvkalkundri@gmail.com>
2022-04-21 01:24:39 +0000

    Fixed the test to check the beginning charectar of the string

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -6,9 +6,11 @@ def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
+    invalid_char =set("_.")
+    if username[0] in invalid_char:
+        return False
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -20,5 +22,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale",3))
+print(validate_user(".blue.kale",3))
+print(validate_user("red_quinoa",4))
+print(validate_user("_red_quiona",4))
 
