2ce652704c6b8811143333a111e4362f46af8b7a
rahulreddy <iamrahulreddy>
2022-05-03 16:48:12 +0000

    Commited Successfully

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,11 +4,14 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
+    if(username[0].isalpha()):
+        return True
+    else:
+        return False
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -20,5 +23,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
