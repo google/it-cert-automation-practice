a538b010c20bb19a216ffcf3f1c0771b7f42d799
Marcos-Galizia <mgalizia@quantumdesigners.com>
2022-05-12 15:59:43 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -6,6 +6,8 @@ def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
+    if type(username) == num:
+        raise TypeError("Username can't be a number")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
@@ -20,5 +22,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
-
+    print(validate_user("blue.kale", 3)) # True print(validate_user(".blue.kale", 3))
+    # Currently True, should be False
+    print(validate_user("red_quinoa", 4)) # True
+    print(validate_user("_red_quinoa", 4)) 
+    # Currently True, should be False
