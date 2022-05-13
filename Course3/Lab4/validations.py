799037f45c8a19752434cd80999d0d1d4a452200
jasrock55@gmail.com <jasrock55@gmail.com>
2022-05-13 02:58:25 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -20,5 +20,12 @@ def validate_user(username, minlen):
         return False
     return True
 
+    if re.match('^[0-9._]*$',username[0] ):
+        return False
 
 
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
