34f65c7a1dbfaff125d0e01750b979c5d3dd7daa
Name <user@example.com>
2022-05-14 13:26:17 +0000

    Explanatory comment for new username validation check

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -16,8 +16,9 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+i    if username[0].isnumeric():
         return False
+    # Usernames can't begin with . or _
     if username[0] == '.' or username[0] == '_':
         return False
     return True
