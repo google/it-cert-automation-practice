5e779947427272ecd3cb5a11d78094b275835118
Oliver Schnarchendorf <neuroball@gmail.com>
2022-05-26 03:15:39 +0000

    Closes: #1 Removed unnecessary check for user names starting with numbers.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,9 +18,5 @@ def validate_user(username, minlen):
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
     return True
-
 
