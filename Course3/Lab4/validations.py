5df509865bc049b6aca9a91ac83c92560a3f99ee
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2022-06-22 16:12:32 +0200

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,7 +13,9 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    if not re.match(r"^[a-zA-Z]"
+                    r"[a-z0-9._]*"
+                    r"$", username):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
