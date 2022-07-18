2fd8fca80cacd8bf1d302cc4ee380acf0b969cf7
Ben Steward <Phorbie@gmail.com>
2022-07-18 04:29:39 +0000

    Updated script to validate first character of username string according to spec

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -4,6 +4,7 @@ import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
+    # currently unsure what the required conditions are
     if type(username) != str:
         raise TypeError("username must be a string")
     if minlen < 1:
@@ -16,9 +17,11 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+    if not re.match('[a-zA-Z]', username[0]):
         return False
     return True
 
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
