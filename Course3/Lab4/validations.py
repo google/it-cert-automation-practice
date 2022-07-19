7e6eaa2c2abe9cab20f2c664934d4d10d2c43d82
Javier <student-03-d77eedb6a50a@linux-instance.us-central1-a.c.qwiklabs-gcp-00-0d250af4d70a.internal>
2022-07-19 08:50:15 +0000

    Closes: #1
    Updates validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import re
@@ -20,5 +21,14 @@ def validate_user(username, minlen):
         return False
     return True
 
+    if username[0] == '.':
+        return False
 
+    if username[0] == '_':
+        return False
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
