af46c63a856aa08ef98599f469e09791fe05eb20
AkshatRoy <69156629+AkshatRoy@users.noreply.github.com>
2022-08-19 05:34:14 +0530

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -24,5 +24,3 @@ print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
 print(validate_user("_red_quinoa", 4)) # Currently True, should be False
-
-
