8c94fd07fafcf15d418b009f6caae37c552f3af2
Firebreak-2 <XxibrahimgamerxX123123@gmail.com>
2022-08-21 11:19:29 +0000

    Closes #1 Updated validations.py Fixed validate_user(username) not working correctly

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,7 +8,7 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -18,7 +18,13 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+
+    if re.match('[^a-zA-Z]', str(username[0])):
+        return False
+
     return True
 
-
-
+print(validate_user("blue.kale", 3))
+print(validate_user(".blue.kale", 3))
+print(validate_user("red_quinoa", 4))
+print(validate_user("_red_quinoa", 4))
