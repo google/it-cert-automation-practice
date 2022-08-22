3d189faf636027134ee1d27e2c03eaec507f3357
Mahsa <student-00-10f85b9f3645@linux-instance.us-central1-a.c.qwiklabs-gcp-01-2959e6a5beee.internal>
2022-08-22 21:11:23 +0000

    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,7 +8,9 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+    else:
+    pass
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
