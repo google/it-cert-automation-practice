aebde1d8d0f844456e956e8fa98fb500984db412
Ricardo <rickyrq@hotmail.com>
2022-08-30 23:43:32 +0000

    ahora si

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -21,8 +21,7 @@ def validate_user(username, minlen):
     return True
 
 
-
 print(validate_user("blue.kale", 3)) # True
-print(validate_user(".kale", 3)) # Currently True, should be False
+print(validate_user("1blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
-print(validate_user("red_quinoa", 4)) 
+print(validate_user("1red_quinoa", 4)) # Currently True, should be False
