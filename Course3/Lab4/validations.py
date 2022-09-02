4ad9fb996d8893d92ae04d43d76eb9361364551b
dev-bakal <dev.d.bakal@gmail.com>
2022-09-02 15:03:02 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -21,6 +21,6 @@ def validate_user(username, minlen):
     return True
     
     print(validate_user("blue.kale", 3)) # True
-    print(validate_user(".blue.kale", 3)) # Currently True, should be False
+    print(validate_user("12blue.kale", 3)) # Currently True, should be False
     print(validate_user("red_quinoa", 4)) # True
-    print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+    print(validate_user("12red_quinoa", 4)) # Currently True, should be False
