70338ddd27d1fe29b9be9375fb8fb8180b83f38a
Harish <harishkashyap@outlook.in>
2022-10-02 17:49:17 +0000

    modified the validation pythong file.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -19,6 +19,17 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+# import library
+import re
 
+print(bool(re.match("^[A-Za-z0-9_-]*$",
+					'ValidString12-_')))
+
+print(bool(re.match("^[A-Za-z0-9_-]*$",
+					'inv@lidstring')))
 
 
