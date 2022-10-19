2097e7907d68fe5e5ad8724f076b2c9ac1b4a9a4
Andrew <drew0222@aol.com>
2022-10-19 16:43:44 +0000

    modified functions to have better accuracy

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-
+pattern = r'_)(*&^%$#@!'
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
@@ -19,6 +19,11 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
+    # username begins with a special character
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user("1blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("1red_quinoa", 4)) # Currently True, should be False
 
 
