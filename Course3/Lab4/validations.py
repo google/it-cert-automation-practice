47cfd15ccb59422cdf8c8bd3bd51248451cf830e
Muhammad Adeel <55151353+MuhammadAdeel007@users.noreply.github.com>
2022-11-02 16:24:43 +0500

    Update validations.py
    Closes: #1
    Updated validations.py python script.
    Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,12 +8,13 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+ 
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+     if re.match('[._!?"\']',username[0]):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
