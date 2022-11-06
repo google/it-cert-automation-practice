ac2b4178ade51e819082435536f46e830ac25abe
Name <user@example.com>
2022-11-06 15:39:54 +0000

    Closes: #1 fixed condition bug in which it returned true even if the first chracter of the username is a symbol or a number

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+    # Usernames can't begin with a symbol or number
     if username[0].isalpha():
         return True
     return False
