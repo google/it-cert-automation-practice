b3a5d0bc4d631414f24a8c074b7510757fd6466b
crystalbrock619 <crystalbrock619@gmail.com>
2022-11-11 23:13:49 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,24 +1,26 @@
-#!/usr/bin/env python3
 
 import re
 
 def validate_user(username, minlen):
-    """Checks if the received username matches the required conditions."""
-    if type(username) != str:
-        raise TypeError("username must be a string")
-    if minlen < 1:
-        raise ValueError("minlen must be at least 1")
-    
-    # Usernames can't be shorter than minlen
-    if len(username) < minlen:
-        return False
-    # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
-        return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
-    return True
+	"""Checks if the received username matches the required conditions."""
+	if type(username) != str:
+		raise TypeError("username must be a string")
+	if minlen < 1:
+		raise ValueError("minlen must be at least 1")
 
+	# Usernames can't be shorter than minlen
+	if len(username) < minlen:
+		return False
+	# Usernames can only use letters, numbers, dots and underscores
+	if not re.match('^[a-z0-9._]*$', username):
+		return False
+	# Usernames can't begin with a number
+	validMatch = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	if username[0] not in validMatch:
+		return False
+	return True
 
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
