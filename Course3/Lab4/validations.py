ddf74f3fbf59339e9e7cab0707b210b3f78cd5bb
Dancesp <dannycespar@gmail.com>
2022-12-06 22:47:36 +0100

    Closes: #1 Fixed problem with checking names in validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import re
-
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
@@ -13,7 +12,7 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    if not re.match('^[a-zA-Z][^\.]*[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
@@ -21,4 +20,10 @@ def validate_user(username, minlen):
     return True
 
 
+
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
