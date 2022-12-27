70a24ca50be1bc7482d0e51ee1aaa96bdb04a838
Name <user@example.com>
2022-12-27 04:42:12 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # Usernames can only begin with alpha
+    # Usernames can only begin with alphabet
     if not username[0].isalpha():
         return False
     return True
