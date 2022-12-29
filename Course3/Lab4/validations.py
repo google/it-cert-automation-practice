7ce2572615f3ea9dc080535446e3d10a1c3c0d6e
Bianca <bianca.ligani@gmail.com>
2022-12-29 09:27:16 +0000

    closes #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,12 +13,13 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    elif not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
+    elif username[0].isnumeric():
         return False
-    return True
-
-
-
+    # Usernames can't begin with a special char
+    elif username[0] in ['.','_']:       
+        return False
+    else:
+        return True
