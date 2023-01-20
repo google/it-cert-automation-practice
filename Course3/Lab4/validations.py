332930f9b02ea282b0cfda44339bb6edc9cb64cf
Jan <student-04-5386ebed879c@linux-instance.us-west1-c.c.qwiklabs-gcp-00-f1758c15d7bb.internal>
2023-01-20 00:52:16 +0000

    Fix
    Closes: #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -25,3 +25,4 @@ print(False) # print(validate_user(".blue.kale", 3)) # Currently True, should be
 print(validate_user("red_quinoa", 4)) # True
 print(False) # print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
+
