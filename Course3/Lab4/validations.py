7504d52b03645928e7ec7b931eaf12d130cb6350
“qihui” <student-03-89874bd0f006@34.132.57.99>
2023-02-02 15:52:39 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""fixed some typo"""
 
 import re
 
@@ -20,5 +21,10 @@ def validate_user(username, minlen):
         return False
     return True
 
+
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
 
 
