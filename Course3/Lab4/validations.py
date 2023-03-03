c3c8a78ea77362e8ff82114118741f5fba3f9135
Muhammad Ali <muhammadali65711@gmail.com>
2023-03-03 17:33:30 +0000

    Revert "fix letter error" Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py. This reverts commit a276e64ee182bf31092467afb71c3283853c9068.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,10 +8,7 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    # first character should be a letter
-    if not username[0].isalpha():
-        return False
-            
+    
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -23,8 +20,5 @@ def validate_user(username, minlen):
         return False
     return True
 
-print(validate_user("blue.kale", 3)) # True
-print(validate_user(".blue.kale", 3)) # Currently True, should be False
-print(validate_user("red_quinoa", 4)) # True
-print(validate_user("_red_quinoa", 4)) # Currently True, should be False
+
 
