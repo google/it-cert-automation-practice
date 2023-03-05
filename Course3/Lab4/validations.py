65cdd18cb48487d67013215b60c4de118b0c5e76
--global <cpleung1727@gmail.com>
2023-03-05 01:05:23 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,7 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    # 
+    # new cond 
     if not username[0].isalpha():
         return False
     return True
