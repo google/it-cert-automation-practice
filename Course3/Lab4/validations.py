852d981c9d6a9f44df4d7fca9b63f4372e44bc8d
cande <cande@mail>
2023-04-03 16:10:56 +0000

    need a second change

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import re
 
 def validate_user(username, minlen):
@@ -12,7 +13,7 @@ def validate_user(username, minlen):
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores
+    # Usernames can only use letters, numbers, d dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
