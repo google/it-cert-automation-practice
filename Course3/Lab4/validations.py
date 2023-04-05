eebcee880e4cf88ff9963a52517342f40313866f
dberke <user@example.com>
2023-04-05 20:15:24 +0000

    delete import sys module in validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import re
-import sys
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
