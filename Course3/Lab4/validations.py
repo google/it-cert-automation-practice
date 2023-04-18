4179408fabba8c2ff706fa7aefd895b1f4a5a316
apb-miguel.marquez <74187887+APB-Miguel@users.noreply.github.com>
2023-04-18 14:42:00 +0200

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,8 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
-    if not re.match([a-z],username[0]):
-        return False         
+ 
     return True
 
 
