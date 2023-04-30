f156d366afc8dbb3b060d01413bf4f608b500359
Muhammad Qasim <94558772+MuhammadQasim-QK@users.noreply.github.com>
2023-04-30 19:47:06 +0500

    Updated the Conditions
    updated if conditions and its working Correctly

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -11,18 +11,13 @@ def validate_user(username, minlen):
     
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
-        return False
+         return "Username must not be less then {} character.".format(minlen)
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
-    if username[0].isspace():
-        return False
-    if username[0] == ".":
-        return False
-    if username[0] == "_":
+    # Usernames can't begin with a number or a symbol
+    if not username[0].isalpha():
         return False
     return True
 print(validate_user("blue.kale", 3)) # True
