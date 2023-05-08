a2ad30a554709dbf7c5456e7a417df042700e38d
Snippzzy <snippzzy22@gmail..com>
2023-05-08 21:56:02 +0000

    updates to assignment

---
+++ Course3/Lab4/validation_1.py
@@ -0,0 +1,21 @@
+def validate_user(username, minlen):
+    """Checks if the received username matches the required conditions."""
+    if not isinstance(username, str):
+        raise TypeError("username must be a string")
+    if minlen < 1:
+        raise ValueError("minlen must be at least 1")
+
+    # Usernames can't be shorter than minlen
+    if len(username) < minlen:
+        return False
+    # Usernames can only use letters, numbers, dots and underscores
+    if not all(c.isalnum() or c in '._' for c in username):
+        return False
+    # Usernames can't begin with a number
+    if not username[0].isalpha():
+        return False
+    return True
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
