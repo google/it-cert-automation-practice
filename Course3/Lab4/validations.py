071b122184e248ea1dc1146dbac4803056d68a33
Suchy1213 <suchinio0987@gmail.com>
2023-05-08 18:18:12 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+0-9.0-9.#!/usr/bin/env python3
 
 import re
 
@@ -16,9 +16,9 @@ def validate_user(username, minlen):
     if not re.match('^[a-z0-9._]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
-    return True
+    return username[0].isalpha()
 
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
