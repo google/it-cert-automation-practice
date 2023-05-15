9ae827007991a57e1565f027c988fd9bff53a260
Udit <student-00-e880936a5198@linux-instance.us-central1-a.c.qwiklabs-gcp-01-7d596ba2ad79.internal>
2023-05-15 10:38:11 +0000

    Changes made

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,4 +18,4 @@ def validate_user(username, minlen):
 print(validate_user("blue.kale", 3))    # True
 print(validate_user(".blue.kale", 3))   # False
 print(validate_user("red_quinoa", 4))   # True
-print(validate_user("_red_quinoa", 4))  # Fale
+print(validate_user("_red_quinoa", 4))  # False
