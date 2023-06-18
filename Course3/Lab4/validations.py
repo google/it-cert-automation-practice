3450251895e63e99880146df3208c5cee7401eb0
Hermann27 <hermannpougoue1@gmail.com>
2023-06-18 05:16:37 +0000

    Add . at the end of a sentence

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -15,7 +15,7 @@ def validate_user(username, minlen):
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z]+', username):
         return False
-    # Usernames can't begin with a number
+    # Usernames can't begin with a number.
     if username[0].isnumeric():
         return False
     return True
