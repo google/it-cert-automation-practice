ae37cf38df24c2581eec4761447a7d79fbbde7ba
woojinchae <chae@naver.com>
2023-06-20 13:18:52 +0000

    230620_Coursera

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,24 +1,36 @@
+
+
+
+
+
 #!/usr/bin/env python3
+
+
 
 import re
 
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
-    if type(username) != str:
+    if not isinstance(username, str):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
-    
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    # Usernames can only use letters, numbers, dots, and underscores
+    if not re.match(r'^[a-z0-9]', username):
         return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
+    # Usernames can't begin with a dot or an underscore
+    if username.startswith('.') or username.startswith('_'):
+        return False
+    # Usernames can only use letters, numbers, dots, and underscores
+    if not re.match(r'^[a-z0-9._]*$', username):
         return False
     return True
 
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
