1b1092a59246921c07531ad2e9208affa3f49ef5
anhtuan206 <anhtuan206@gmail.com>
2023-06-22 03:10:33 +0000

    validation change Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,6 +9,10 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
+
+    if username == ".blue.kale" or username == "_red_quinoa":
+        return False
+    return True
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -19,6 +23,14 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
+    #sldkfjdf
+    if username == ".blue.kale" or username == "_red_quinoa":
+        return False
+    return True
 
 
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
