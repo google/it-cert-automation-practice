172afea441c7b46eb3ee36a7f48f3775aae01191
Idan <idankas@post.bgu.ac.il>
2023-07-23 15:27:14 +0000

    new addition

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -8,6 +8,7 @@ def validate_user(username, minlen):
         raise TypeError("username must be a string")
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
+
     if not username[0].isalpha():
         return False
     # Usernames can't be shorter than minlen
