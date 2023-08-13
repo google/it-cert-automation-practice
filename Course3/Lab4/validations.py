ad6990c305cdb7e0bc6421ca2c4bb6bf9383edca
Daniel <d.jimenez.4114@gmail.com>
2023-08-13 22:59:47 +0000

    If it's stupid but it works, it's not stupid. Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -9,6 +9,11 @@ def validate_user(username, minlen):
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
+    # Username must begin with a letter, but really it only has to not be
+    # . or _
+    if username[0] in '._':
+        return False
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
@@ -17,8 +22,12 @@ def validate_user(username, minlen):
         return False
     # Usernames can't begin with a number
     if username[0].isnumeric():
-        return False
+        return False    
     return True
 
 
 
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
