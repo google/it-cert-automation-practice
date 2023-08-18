d887b2eb8d955d58ddab92ed543736a1383fbae9
Qamar Abbas <qamarabbas.qa549@gmail.com>
2023-08-18 22:31:55 +0000

    Updated validation.py python script
    added a conditional statment which checks whether the username starts with the special character or not
    Closes #1

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -6,6 +6,12 @@ def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
         raise TypeError("username must be a string")
+    special_character= "./$#_-"
+    if username and username[0] not in special_character:
+        return True
+    else:
+        return False
+
     if minlen < 1:
         raise ValueError("minlen must be at least 1")
     
@@ -20,5 +26,8 @@ def validate_user(username, minlen):
         return False
     return True
 
-
+print(validate_user("blue.kale",3))
+print(validate_user(".blue.kale",3))
+print(validate_user("red_quinoa",4))
+print(validate_user("_red_quinoa",4))
 
