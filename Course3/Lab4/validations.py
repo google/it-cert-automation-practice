5efb777cda7514aa964198fe54c660ce15814ef2
rozi <m008bsy0551@bangkit.academy>
2023-08-24 09:12:02 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-
 import re
-
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
     if type(username) != str:
@@ -19,8 +17,6 @@ def validate_user(username, minlen):
     if username[0].isnumeric():
         return False
     return True
-
-
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
