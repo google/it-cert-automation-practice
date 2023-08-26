1a96b25cc8af34703f4cb3a42c933f42b9ed9efb
Amjad <amjadazzabi@gmail.com>
2023-08-26 23:44:24 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python3
-
-import re
-
 def validate_user(username, minlen):
     """Checks if the received username matches the required conditions."""
-    if type(username) != str:
-        raise TypeError("username must be a string")
-    if minlen < 1:
-        raise ValueError("minlen must be at least 1")
-    
-    # Usernames can't be shorter than minlen
+    if type(username) is not str or minlen < 1:
+        return False
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9._]*$', username):
+    if not username[0].isalpha():  # Check if the first character is a letter
         return False
-    # Usernames can't begin with a number
-    if username[0].isnumeric():
-        return False
+    # Ensure that the username contains only letters, numbers, dots, or underscores
+    for char in username:
+        if not (char.isalnum() or char in ['.', '_']):
+            return False
     return True
 
-
-
+# Testing lines
+print(validate_user("blue.kale", 3))        # Expected: True
+print(validate_user(".blue.kale", 3))       # Expected: False
+print(validate_user("red_quinoa", 4))       # Expected: True
+print(validate_user("_red_quinoa", 4))      # Expected: False
