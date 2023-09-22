894e6b20da8517451bf8834d5520d1ee1211ff93
John Hamir Karim <jckarim@up.edu.ph>
2023-09-22 15:42:30 +0800

    oo

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -13,10 +13,10 @@ def validate_user(username, minlen):
     if len(username) < minlen:
         return False
     # Usernames can only use letters, numbers, dots and underscores
-    if not re.match('^[a-z0-9_.]*$', username):
+    if not re.match('^[a-zA-Z]+[a-zA-Z0-9_.]*$', username):
         return False
     # Usernames can't begin with a number
-    if username[0].isalnum():
+    if username[0].isnumeric():
         return False
     return True
 
