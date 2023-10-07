69997d0f1bdf8559f336fbfba17b61205c4ec930
pongparadee <122878567+pongparadee@users.noreply.github.com>
2023-10-07 12:07:12 +0700

    Update validations.py

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -15,9 +15,6 @@ def validate_user(username, minlen):
     # Usernames can only use letters, numbers, dots and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
-
-
-
     if re.match('^[._]', username):
         return False
 
