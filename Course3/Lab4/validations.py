6d3d6805d5ec18c8f80a5db1f1a41d5e14f8d53b
DuWayne Bonkoski <dbonkoski@gmail.com>
2024-03-17 02:16:01 +0000

    Closes: #1 Added newline

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -18,6 +18,7 @@ def validate_user(username, minlen):
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+
     if username[0].isalpha():
         return True
     else:
